11e663c6e20d9d5ef11c0f661e352353eca37dcf
Еров Виктор Владимирович <v.erov@etpgpb.ru>
2024-04-22 16:45:08 +0300

    HW1 is completed

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
 module github.com/fixme_my_friend/hw01_hello_otus
 
 go 1.19
+
+require golang.org/x/example v0.0.0-20230726192220-6088d5b0e079 // indirect
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	inStr := "Hello, OTUS!"
+	fmt.Print(stringutil.Reverse(inStr))
 }
